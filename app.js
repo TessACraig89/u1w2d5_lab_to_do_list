0cfa26a0481c0149c278117e4bc3bb6a74b5cebc
Tess Craig <tessashleycraig89@gmail.com>
2017-12-15 13:51:33 -0600

    Commit 3 - changed all js to jquery

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
-const addButton = document.getElementById('addButton')
+//creates listItems array
+const listItems = [];
 
-let list = document.getElementById('list')
+//adds click event listener to addButton that triggers pushToArray function
+$( '#addButton').click(function() {
+  pushToArray();
+});
 
-let listItems = [];
-
-
-addButton.addEventListener('click', pushToArray = () => {
-  let postBoxValue = document.getElementById('postBox').value;
-    listItems.unshift(postBoxValue);
-    postBoxValue = null;
-    publishPost();
-})
-
+//creates publishPost function
+// stores <ul>
+// creates and stores list element
+// stores listItems array[0] to content var
+//append content into list element
+//append list element into list
 const publishPost = () => {
-    let liElement= document.createElement('LI');
-    let content = listItems[0];
-    liElement.append(content);
-    list.appendChild(liElement);
-    list.insertBefore(liElement, list.childNodes[0])
+  let $list = $('#list');
+  let $listElement = $("<li>");
+  let $content = listItems[listItems.length-1];
+  $listElement.append($content);
+  $list.append($listElement);
+
 }
+//creates pushToArray function
+// stores input value
+//push postBoxValue into list items array
+//set postBox value to null
+//call publishPost function
+const pushToArray = () => {
+  let $postBoxValue = $('#postBox').val();
+  listItems.push($postBoxValue);
+  $('#postBox').val(null);
+  publishPost();
+}
+
+
+
+/////////////////////////////////
+// const addButton = document.getElementById('addButton')
+//
+// let list = document.getElementById('list')
+//
+// let listItems = [];
+//
+//
+// postButton.addEventListener('click', pushToArray = () => {
+//   let postBoxValue = document.getElementById('postBox').value;
+//     listItems.unshift(postBoxValue);
+//     postBoxValue = null;
+//     publishPost();
+// })
+//
+// const publishPost = () => {
+//     let liElement= document.createElement('LI');
+//     let content = madePosts[0];
+//     liElement.append(content);
+//     list.appendChild(liElement);
+//     list.insertBefore(liElement, list.childNodes[0])
+// }
